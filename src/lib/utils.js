d7d599370091a774a08113f100f704f755639c04
SHDPIXEL <shdpixel@gmail.com>
2025-02-06 18:45:18 +0530

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL for the API
-const BASE_URL = "http://13.234.31.217"; // Replace with your server's base URL
+const BASE_URL = "https://ecoback.demo.shdpixel.com"; // Replace with your server's base URL
 
 // Create an Axios instance
 const API = axios.create({
